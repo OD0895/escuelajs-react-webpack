7a4f3960085459f5def618fcb83d3c3aa65ec271
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-01-25 16:27:07 -0600

    Implementation Hooks and consumo de Api

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,41 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-import '../assets/styles/App.scss'
+import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    //const [ videos, setVideos ] = useState([]);
+    const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+            .then(response => response.json())
+            .then(data => setVideos(data));
+    }, []);
 
-        <Categories title="1 Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="2 Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="2 Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    return(
+        <div className="App">
+            <Header />
+            <Search />
+            {videos.mylist.length > 0 &&
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
+            <Categories title="Tendencias">
+                <Carousel>
+                    {videos.trends.map(item =>
+                        <CarouselItem key={item.id} {...item}/>
+                    )}
+                </Carousel>
+            </Categories>
 
-        <Footer />
-    </div>
-);
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
