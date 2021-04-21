1ee48b0f7295fc77ced3be226fbb53e52b2dd13f
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-04-20 19:55:30 -0500

    :construction: [ADD] libreria nodemon para ejecutar en live, dotenv para variables de entorno

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import config from './config';
+
+const { env, port } = config;
 
 const app = express();
 
+if( env === 'development')
+  console.log('Development config');
+  
 app.get('*', (req, res) => {
   res.send({ hello: 'express' });
 });
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if(err) console.log(err);
   else console.log('Server running on port 3000');
-})
+});
