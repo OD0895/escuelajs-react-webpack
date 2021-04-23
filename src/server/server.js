7a722e2e35899ee03ce24e5bd559538d9c8a1067
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-04-22 20:00:22 -0500

    :construction: [ADD] Integracion de Webpack con Express

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,29 @@
 import express from 'express';
 import config from './config';
+import webpack from 'webpack';
 
 const { env, port } = config;
 
 const app = express();
 
-if( env === 'development')
+if (env === 'development') {
   console.log('Development config');
-  
+  // eslint-disable-next-line global-require
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webPackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { port: port, hot: true };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webPackHotMiddleware(compiler));
+}
+
 app.get('*', (req, res) => {
   res.send({ hello: 'express' });
 });
 
 app.listen(port, (err) => {
   if(err) console.log(err);
-  else console.log('Server running on port 3000');
+  else console.log(`Server running on port ${port}`);
 });
